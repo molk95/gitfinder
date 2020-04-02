b968cb57e1c134d5785ad3e0d2c1b8556c1b12da
molk95 <molk.saouabi@gmail.com>
2020-04-02 18:10:44 +0200

    HTTP Requests & Updating State

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react' ;
-import './App.css';
-import Navbar from './components/layout/Navbar';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UsersSection from "./components/users/UsersSection";
+import axios from "axios";
+import "./App.css";
 
-import UsersSection from './components/users/UsersSection'
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
 
- class App extends Component {
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
-
-
     return (
       <div>
-         <Navbar
-          title='GitHub Finder'
-          icon='fab fa-github '
-          />
-         <div className= "container">
-         <UsersSection />
-         </div>
-
-        
+        <Navbar title="GitHub Finder" icon="fab fa-github " />
+        <div className="container">
+          <UsersSection loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
