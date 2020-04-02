323cc7337f89544a76af7db334aab2666f1376f7
molk95 <molk.saouabi@gmail.com>
2020-04-02 17:53:19 +0200

    Functional Component

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
+import React from "react";
+import PropTypes from 'prop-types' 
 
- class UserCard extends Component {
-  
-    render() {
-        const {login,avatar_url,html_url}= this.props.user;
-        return (
-            <div className="card text-center" >
-                <img src={avatar_url} alt="" className="round-img" style={{ width:"60px" }} />
-        <h3>{login}</h3>
-        <div>
-            <a href={html_url} className="btn btn-dark btn-sm my-1" >More</a> 
-        </div>
-            </div>
-            
-        )
-    }
+const UserCard = ({user: { login, avatar_url, html_url }}) => {
+
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+UserCard.propTypes ={
+    user:PropTypes.object.isRequired
 }
-
-export default UserCard
+export default UserCard;
