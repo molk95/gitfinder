6ee8f85886f73a8f99f81acae9a6fe42f82b7077
molk95 <molk.saouabi@gmail.com>
2020-04-09 16:34:28 +0200

    Clear Users From State

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
   };
 
   // async componentDidMount() {
@@ -22,20 +22,28 @@ class App extends Component {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  searchUsers = async text => {
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}`
     );
     this.setState({ users: res.data.items, loading: false });
   };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div>
         <Navbar title="GitHub Finder" icon="fab fa-github " />
-        <Search searchUsers={this.searchUsers} />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={users.length > 0 ? true : false}
+        />
         <div className="container">
-          <UsersSection loading={this.state.loading} users={this.state.users} />
+          <UsersSection loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
   onChange = (e) => {
     this.setState({
@@ -20,6 +22,7 @@ class Search extends Component {
     this.setState({ text: "" });
   };
   render() {
+    const {showClear, clearUsers} = this.props
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -35,6 +38,14 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
